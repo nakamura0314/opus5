a151c6357e4a52d5b0d10315a8f5fe2ed1c5ad9e
takaho0314 <takaho0314@gmail.com>
2022-01-27 23:45:08 +0900

    first

---
+++ mnist_practics.py
@@ -0,0 +1,21 @@
+from sklearn import datasets, svm
+from sklearn.model_selection import train_test_split
+
+# サンプル画像データのロード
+# mnistは、手書きの数字0~9に正解ラベルが与えられているデータセット
+mnist = datasets.fetch_openml('mnist_784', data_home='image/')
+X = mnist.data / 255
+y = mnist.target
+
+# 訓練用データとテスト用データに分ける
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, train_size=1000, test_size=300
+)
+
+# 訓練用データで学習
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+
+# テスト用データで実際に試す
+score = clf.score(X_test, y_test)
+print(score)
